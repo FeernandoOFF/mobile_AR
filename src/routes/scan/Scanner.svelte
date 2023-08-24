45eba937fb1215bcac73835147ae1df3266c1bc9
Fernando <ferobregon03@gmail.com>
2023-08-24 13:42:39 +0100

    [Added] Background color for themes

--- src/routes/scan/Scanner.svelte
+++ src/routes/scan/Scanner.svelte
@@ -36,7 +36,7 @@
 					cylinder
 				</label>
 			</div>
-			<a-scene background="color: #FAFAFA">
+			<a-scene>
 				<a-entity camera />
 				<a-box position="-1 0.5 -3" rotation="0 45 0" color={box} shadow />
 				<a-sphere
