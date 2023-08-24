c2bb9031060e0f9e84dffa1e8ba365cb48ae314f
Fernando <ferobregon03@gmail.com>
2023-08-24 10:40:26 +0100

    [Added] Project scaffolding

---
+++ src/routes/scan/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Block, Navbar, Page } from 'konsta/svelte';
+</script>
+
+<Page>
+	<Navbar title="Scan your content" />
+	<Block>
+		<p>You can scan something here:</p>
+	</Block>
+	<div class="w-full max-w-sm h-[30vh] bg-gray-400" />
+</Page>
